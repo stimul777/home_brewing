5e48e2f08a4f734d9d1cdc0add8bd134d548c493
Юрий Девляшов <YYDEVLYASHOV@BG.loc>
2021-02-15 16:56:58 +0300

    configured router

--- src/router/router.js
+++ src/router/router.js
@@ -14,10 +14,21 @@ const routes = [
   {
     path: '/login',
     name: 'login',
+    meta: { layout: 'empty' },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main' },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Categories.vue')
   }
 ]
 
