1634f518fdffb87688c19ef9801f7fd78b0ace09
Юрий Девляшов <YYDEVLYASHOV@BG.loc>
2021-02-17 16:39:03 +0300

    added components, added routes

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id='app'>
-    <component :is='layout'></component>
-    <!-- <div id='nav'>
-      <router-link to='/'>Home</router-link>|
-      <router-link to='/about'>About</router-link>
-    </div>-->
+    <component :is='layout'>
+      <router-view />
+    </component>
   </div>
 </template>
 
 <script>
-import EmptyLayout from './layouts/EmptyLayout'
-import MainLayout from './layouts/MainLayout'
-
+import EmptyLayout from '@/layouts/EmptyLayout'
+import MainLayout from '@/layouts/MainLayout'
 export default {
+  computed: {
+    layout() {
+      // в зависимости от роута перерисовать layout
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    },
+  },
   components: {
     EmptyLayout,
     MainLayout,
   },
-
-  computed: {
-    layout() {
-      // console.log('!!!', this.$route.meta)
-      return (this.$route.meta.layout || 'empty') + '-layout'
-    },
-  },
 }
 </script>
 
-<style>
+
+<style >
 @import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
 </style>
